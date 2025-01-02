f1eedffa2e896be35fc1d4412de416d8426da3bb
Admin <chirag.soni@anant.co.in>
2025-01-02 16:30:45 +0530

    new api Add

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express()
 const port = 8080
 
 app.use(express.static(path.join(__dirname + "/public")));
+app.get("api",()=>{
+  console.log("hello")
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
